3b11fd255f9e5a6186058869d191df713f1b4eef
Ahmed Abbas <ahmedabb101@gmail.com>
2021-06-10 11:45:31 +0300

    projects updated

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,6 +4,9 @@ import PomoWatch from '../../images/pomowatch.png'
 import QuoteGenerator from  '../../images/quotegenerator.png';
 import PasswordGenerator from '../../images/passwordgenerator.png';
 import SnakeGame from '../../images/snakegame.png';
+import SudokuSolver from '../../images/sudokusolver.png'
+import ChatApp from '../../images/chatapp.png'
+import SortingVisualizer from '../../images/sortingvisualizer.png'
 import { Button } from '../ButtonElement';
 import { AiOutlineGithub } from 'react-icons/ai';
 import { BiWorld } from 'react-icons/bi';
@@ -14,6 +17,30 @@ const Projects = () => {
 			<ProjectsContainer id="projects">
 				<ProjectsH1>Projects</ProjectsH1>
 				<ProjectsGrid>
+					<ProjectsCard>
+						<ProjectsImg src={SortingVisualizer}/>
+						<ProjectsH2>Sorting Visualizer</ProjectsH2>
+						<ProjectsP>Visualizations of sorting algorithms, built with React</ProjectsP>
+						<BtnWrapper>
+							<Button href="https://sort-visual.netlify.app/" target="_blank" style={{fontSize: "18px", padding:"12px 20px", marginRight:"5px"}}><BiWorld style={{fontSize: "20px"}} />Site</Button>
+							<Button href="https://github.com/ahmedabb104/Sorting-Visualizer" target="_blank" style={{fontSize: "18px", padding:"12px 20px"}}><AiOutlineGithub style={{fontSize: "20px"}}/>Code</Button>
+						</BtnWrapper>
+					</ProjectsCard>
+					<ProjectsCard>
+						<ProjectsImg src={ChatApp}/>
+						<ProjectsH2>Chat App</ProjectsH2>
+						<ProjectsP>Real-time communication via Socket.io, built with Node.js</ProjectsP>
+						<BtnWrapper>
+							<Button href="https://ahmeds-chat-app.herokuapp.com/" target="_blank" style={{fontSize: "18px", padding:"12px 20px", marginRight:"5px"}}><BiWorld style={{fontSize: "20px"}} />Site</Button>
+							<Button href="https://github.com/ahmedabb104/Chat-App" target="_blank" style={{fontSize: "18px", padding:"12px 20px"}}><AiOutlineGithub style={{fontSize: "20px"}}/>Code</Button>
+						</BtnWrapper>
+					</ProjectsCard>
+					<ProjectsCard>
+						<ProjectsImg src={SudokuSolver}/>
+						<ProjectsH2>Sudoku Game & Solver</ProjectsH2>
+						<ProjectsP>Solves a sudoku board via a backtracking algorithm</ProjectsP>
+						<Button href="https://github.com/ahmedabb104/Sudoku-Game-and-Solver" target="_blank" style={{fontSize: "18px", padding:"12px 20px"}}><AiOutlineGithub style={{fontSize: "20px"}}/>Code</Button>
+					</ProjectsCard>
 					<ProjectsCard>
 						<ProjectsImg src={PomoWatch}/>
 						<ProjectsH2>PomoWatch</ProjectsH2>
